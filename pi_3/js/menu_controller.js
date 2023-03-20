4839dddf0d0f89dcd2b5d945ca39f1002ad7d646
JosepLlimona <62258739+JosepLlimona@users.noreply.github.com>
2023-03-20 16:16:24 +0100

    Practica 3 preparada
    Arxius pujats i boto creat per accedir a p3

---
+++ pi_3/js/menu_controller.js
@@ -0,0 +1,21 @@
+function start_game(){
+	name = prompt("User name");
+	
+	sessionStorage.setItem("username", name);
+	
+	loadpage("./html/game.html");
+}
+
+function exit (){
+	if (name != ""){
+		alert("Leaving " + name + "'s game");
+	}
+	name = "";
+	document.location.href = "../index.html";
+}
+
+function options(){
+	loadpage("./html/options.html");
+}
+
+
