0cf0198b3f06959a5da22356f44ac79ad39ca4a9
JosepLlimona <josepllimona@hotmail.com>
2023-04-28 16:11:29 +0200

    Puntuacio creada

--- pi_4/js/score_controller.js
+++ pi_4/js/score_controller.js
@@ -1,7 +1,26 @@
 var score = new Vue({
     el: '#score',
     data: {
-        items: localStorage.partides ? JSON.parse(localStorage.partides).slice(0, 10) : []
+        items: localStorage.partides ? JSON.parse(localStorage.partides).slice(0, 10).sort(this.compare) : []
+    },
+    methods: {
+        test: function () {
+            console.log("Works");
+            console.log(this.items);
+        },
     }
 
 })
+
+function compare(a, b) {
+    console.log("Es crida");
+    if (a.level > b.level)
+        return -1;
+    if (a.level < b.level)
+        return 1;
+    if (a.level === b.level && a.score > b.score)
+        return -1;
+    if (a.level === b.level && a.score < b.score)
+        return 1;
+    return 0
+}
